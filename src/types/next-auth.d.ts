6f96c5504a542b2ed2cce0b36cdffb39ceb976bf
PhucPhan <phanhoangphuc8991@gmail.com>
2025-12-01 15:25:46 +0700

    declare typescript

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,30 @@
+interface IUser {
+    _id: string,
+    username: string,
+    email: string,
+    isVerity: boolean,
+    type: string,
+    role: string
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        access_token: string,
+        refresh_token: string,
+        user: IUser,
+        access_expire: number,
+        error: string
+    }
+}
+
+
+declare module "next-auth" {
+    interface Session {
+      user: IUser,
+      access_token: string,
+      refresh_token: string,
+      access_expire: number,
+      error: string,
+    }
+}
+
